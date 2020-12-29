c26e68078e878971904fbe17b3e3588cf2212907
Guy McBride <guyjdmcbride@gmail.com>
2020-12-29 12:57:13 +0000

    use degrees rather than radians Offer user choice of dates and fields for non-complex types

--- plotlyplay.py
+++ plotlyplay.py
@@ -24,7 +24,7 @@ conversions = {
 df = df.astype(conversions)
 
 df["wind_apparent_raw_speed"] = np.absolute(df.wind)
-df["wind_apparent_raw_angle"] = np.angle(df.wind)
+df["wind_apparent_raw_angle"] = np.degrees(np.angle(df.wind))
 
 print(df.info())
 
@@ -32,9 +32,9 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div(
     [
-        html.P("Color:"),
+        html.P("Date:"),
         dcc.Dropdown(
-            id="dropdown",
+            id="date_dropdown",
             options=[
                 {"label": x, "value": x}
                 for x in df.index.map(pd.Timestamp.date).unique()
@@ -42,17 +42,30 @@ app.layout = html.Div(
             value=df.index.map(pd.Timestamp.date).unique()[0],
             clearable=False,
         ),
+        html.P("Field:"),
+        dcc.Dropdown(
+            id="field_dropdown",
+            options=[
+                {"label": x, "value": x}
+                for x in df.select_dtypes(exclude=[complex]).columns
+            ],
+            value=df.select_dtypes(exclude=[complex]).columns[0],
+            clearable=False,
+        ),
         dcc.Graph(id="graph"),
     ]
 )
 
 
-@app.callback(Output("graph", "figure"), [Input("dropdown", "value")])
-def display_date(date):
+@app.callback(
+    Output("graph", "figure"),
+    [Input("date_dropdown", "value"), Input("field_dropdown", "value")],
+)
+def display_date(date, field):
     fig = go.Figure()
     fig.add_trace(
         go.Scatter(
-            y=df.loc[date].wind_apparent_raw_speed,
+            y=df.loc[date][field],
             x=df.loc[date].index,
             mode="markers",
         )
